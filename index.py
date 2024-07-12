2a25f2aec3b9140cf5dfe8dafff95f87b412a39d
Mohammed ABBAKAR <73604525+MohammedABBAKAR@users.noreply.github.com>
2024-07-13 01:48:08 +0200

    short_long_short

---
+++ index.py
@@ -0,0 +1,23 @@
+# Given 2 strings, a and b, return a string of the form short+long+short, 
+# with the shorter string on the outside and the longer string on the inside. 
+# The strings will not be the same length, but they may be empty ( zero length ).
+
+# Hint for R users:
+
+# The length of string is not always the same as the number of characters
+# For example: (Input1, Input2) --> output
+
+
+# ("1", "22") --> "1221"
+# ("22", "1") --> "1221"
+
+
+def short_long_short(a, b):
+    if len(a) < len(b):
+        short = a
+        long = b
+    else:
+        short = b
+        long = a
+    
+    return short + long + short
